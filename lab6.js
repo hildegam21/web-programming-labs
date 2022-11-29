2e43e4abb621e157479e35cd201c3eaf02a2fc1b
Hildegam21 <student@fb.local>
2022-11-29 10:42:09 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -8,11 +8,11 @@ function setRussian() {
     document.getElementById('your-name').innerHTML = 'Ваше имя';
     document.getElementById('input-name').placeholder = 'Введите имя';
 
-    document.getElementById('your-name').innerHTML = 'Ваша фамилия';
-    document.getElementById('input-name').placeholder = 'Введите фамилию';
+    document.getElementById('your-surname').innerHTML = 'Ваша фамилия';
+    document.getElementById('input-surname').placeholder = 'Введите фамилию';
 
-    document.getElementById('your-name').innerHTML = 'Ваш возраст';
-    document.getElementById('input-name').placeholder = 'Введите возраст';
+    document.getElementById('your-age').innerHTML = 'Ваш возраст';
+    document.getElementById('input-age').placeholder = 'Введите возраст';
 
     document.getElementById('ok-button').value = 'ОК';
 
@@ -29,14 +29,39 @@ function setEnglish() {
     document.getElementById('your-name').innerHTML = 'Your name';
     document.getElementById('input-name').placeholder = 'Enter your name';
     
-    document.getElementById('your-name').innerHTML = 'Your surname';
-    document.getElementById('input-name').placeholder = 'Enter your surname';
+    document.getElementById('your-surname').innerHTML = 'Your surname';
+    document.getElementById('input-surname').placeholder = 'Enter your surname';
 
-    document.getElementById('your-name').innerHTML = 'Your age';
-    document.getElementById('input-name').placeholder = 'Enter your age';
+    document.getElementById('your-age').innerHTML = 'Your age';
+    document.getElementById('input-age').placeholder = 'Enter your age';
 
     document.getElementById('ok-button').value = 'OK';
 
     document.getElementById('reset-button').value = 'Reset';
 }
 
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    let userStudent = document.getElementById('student').innerHTML;
+    let userFooter = document.getElementById('footer').innerHTML;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    alert('Приветствую, ' + userStudent);
+    alert(userStudent + ' учатся на ' + userFooter);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    let a = Math.round(Math.random()*255);
+    let c = Math.round(Math.random()*255);
+    let d = Math.round(Math.random()*255);
+    let color_2 = 'rgb(' + a + ',' + c + ',' + d + ')';
+    let e = Math.round(Math.random()*255);
+    let f = Math.round(Math.random()*255);
+    let h = Math.round(Math.random()*255);
+    let color_3 = 'rgb(' + e + ',' + f + ',' + h + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+    document.getElementById('footer').style.backgroundColor = color_2;
+    document.getElementById('student').style.backgroundColor = color_3;
+}
