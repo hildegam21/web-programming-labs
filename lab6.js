37e9965348282c6ed4e28cb0329ca7bc1557df04
Hildegam21 <student@fb.local>
2022-11-29 10:02:03 +0700

    Переводы

---
+++ lab6.js
@@ -0,0 +1,42 @@
+function setRussia() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Жазгул Абдыкаарова. Юлия Королева';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Факультет бизнеса, ФБИ-03, 3 курс, 2022';
+
+    document.getElementById('your-name').innerHTML = 'Ваше имя';
+    document.getElementById('input-name').placeholder = 'Введите имя';
+
+    document.getElementById('your-name').innerHTML = 'Ваша фамилия';
+    document.getElementById('input-name').placeholder = 'Введите фамилию';
+
+    document.getElementById('your-name').innerHTML = 'Ваш возраст';
+    document.getElementById('input-name').placeholder = 'Введите возраст';
+
+    document.getElementById('ok-button').value = 'ОК';
+
+    document.getElementById('reset-button').value = 'Сброс';
+}
+
+function setenghlish() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Zhazgul Abdykaarova. Julia Koroleva';
+
+    let footer = document.getElementById('footer');
+    footer.innerHTML = 'Faculty of Business, FBI-03, 3rd year, 2022';
+
+    document.getElementById('your-name').innerHTML = 'Your name';
+    document.getElementById('input-name').placeholder = 'Enter your name';
+    
+    document.getElementById('your-name').innerHTML = 'Your surname';
+    document.getElementById('input-name').placeholder = 'Enter your surname';
+
+    document.getElementById('your-name').innerHTML = 'Your age';
+    document.getElementById('input-name').placeholder = 'Enter your age';
+
+    document.getElementById('ok-button').value = 'OK';
+
+    document.getElementById('reset-button').value = 'Reset';
+}
+
