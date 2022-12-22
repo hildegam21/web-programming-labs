ba33cb1c4e2ae16fc8a534ab363716f79249782f
Hildegam21 <y.koroleva02@mail.ru>
2022-12-22 18:36:29 +0700

    Круглая дата

--- lab8.js
+++ lab8.js
@@ -1,7 +1,7 @@
 function showDate(params){
     let out = document.getElementById('today');
     let today = new Date();
-    out.innerHTML = today.toLocaleDateString("ru-RU");
+    out.innerHTML = prettyDate(today);
 }
 
 function showDaysCount(params){
@@ -19,6 +19,35 @@ function showTime() {
     let outTime = document.getElementById ('time');
     let currentTime = new Date ();
     outTime.innerHTML = currentTime.toLocaleTimeString('ru');
+}
+
+function showNumber(params){
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    let resultNumber = document.getElementById('resultNumber');
+    let inputNum = document.querySelector('input[type=number]');
+    let num = new Number (inputNum.value); 
+    let anniversary = birthday.getTime () + num * 1000 * 60 * 60 * 24;
+    resultNumber.innerHTML = 'Дата после прибавления: ' + new Date(anniversary).toLocaleDateString("ru-RU");
+}
+
+function prettyDate (d) {
+    let day = d.getDate();
+    let newMonth = ["января", "февраля", "марта", "апреля", "мая", "июня",
+    "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    let month = newMonth[d.getMonth()];
+    let year = d.getFullYear();
+    return day + ' ' + month + ' ' + year + ' ' + 'года';
+}
+
+function showNewDate(params){
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    resultNumber.innerHTML = 'Дата обновленная: ' + prettyDate(birthday);
+}
+
+function showNewTime(params){
+    let 
 }
 
 window.addEventListener('load', showDate);
