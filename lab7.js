727752a2af7c8eff44f72689d50292b6b59c5d8f
Hildegam21 <y.koroleva02@mail.ru>
2022-12-25 14:33:14 +0700

    Ничего

---
+++ lab7.js
@@ -0,0 +1,48 @@
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    let userSex = document.getElementById('sex').value;
+
+    let greeating;
+    if(userAge <= 17){
+        greeting = 'Здравствуй, ';
+        if(userSex == 'female'){
+            greeting += 'девочка '; 
+        }
+        else {
+            greeting += 'мальчик '; 
+        }
+    }
+    else {
+        greeting = 'Здравствуйте, ';
+        if(userSex == 'female'){
+            greeting += 'Госпожа '; 
+        }
+        else {
+            greeting += 'Господин '; 
+        }
+    }
+    alert(greeting + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function makeStairs() {
+    let stairs = document.querySelector('#stairs');
+    let stepCount = 20;
+    for (let i = 0; i<stepCount; i++){
+        stairs.innerHTML += '<div class="step"></div>';
+    }
+
+    let steps = document.querySelectorAll('.step');
+    for(let i = 0; i<steps.length; i++){
+        steps[i].style.left = (30 + 10*i) + 'px';
+        steps[i].style.top = (300 + 80*i) + 'px';
+        steps[i].innerHTML = 'Блок номер ' + i;
+    }
+}
+ 
