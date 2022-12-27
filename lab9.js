881f31715c2618242652afdc22b6af96429c4d0c
Hildegam21 <y.koroleva02@mail.ru>
2022-12-27 09:02:17 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,17 @@ function clickMe(button) {
     else 
         button.innerHTML = 'Щелчок';
 }
+
+function clickMe1(button) {
+    if(button.innerHTML == 'Жазгул')
+        button.innerHTML = 'Абдыкаарова';
+    else 
+        button.innerHTML = 'Жазгул';
+}
+
+function clickMe2(button) {
+    if(button.innerHTML == 'Юлия')
+        button.innerHTML = 'Королева';
+    else 
+        button.innerHTML = 'Юлия';
+}
